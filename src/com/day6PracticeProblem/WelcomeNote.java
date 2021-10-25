27f2ce74079e0f94ee95117771975324407f12a0
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-25 12:39:23 +0530

    Welcome Note Code.

---
+++ src/com/day6PracticeProblem/WelcomeNote.java
@@ -0,0 +1,7 @@
+package com.day6PracticeProblem;
+
+public class WelcomeNote {
+    public static void main(String[] args) {
+        System.out.println("\n\nPlease find solution to the problem statements for Day 6,\nplease switch through branches.");
+    }
+}

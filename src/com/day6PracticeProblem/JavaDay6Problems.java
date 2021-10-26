e7a4a0554c3367b1ae44a183b9abbc65525879ca
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-26 16:08:59 +0530

    Added cod efor printing fibonacci series till user input

--- src/com/day6PracticeProblem/JavaDay6Problems.java
+++ src/com/day6PracticeProblem/JavaDay6Problems.java
@@ -1,7 +1,24 @@
 package com.day6PracticeProblem;
+import java.util.*;
 
 public class JavaDay6Problems {
     public static void main(String[] args) {
-        System.out.println("\n\nPlease find solution to the problem statements for Day 6,\nplease switch through branches.");
+        JavaDay6Problems methodCaller = new JavaDay6Problems();
+        methodCaller.fibonacciSeries();
+    }
+    public void fibonacciSeries() {
+        Scanner fibonacciLengthInput = new Scanner(System.in);
+        System.out.println("Please enter the length of Fibonacci Series want to print for.");
+        int fibonacciInput =  fibonacciLengthInput.nextInt();
+        int number01 = 0;
+        int number02 = 1;
+        System.out.print(number01 + " + " + number02 + " + ");
+        for(int i=0;i < fibonacciInput-2;i++){
+            int number03 = number01 + number02;
+            System.out.print(" + "+number03);
+            number01 = number02;
+            number02 =  number03;
+        }
+
     }
 }

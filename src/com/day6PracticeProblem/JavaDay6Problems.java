a1c08e327186d09ee2b68c2f7c803b53dd05228c
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-27 00:27:38 +0530

    Added code for testing any number for Perfect number.

--- src/com/day6PracticeProblem/JavaDay6Problems.java
+++ src/com/day6PracticeProblem/JavaDay6Problems.java
@@ -1,24 +1,25 @@
 package com.day6PracticeProblem;
 import java.util.*;
 
-public class JavaDay6Problems {
+public class JavaDay6Problems{
+     void checkPerfectNumber() {
+         Scanner scanInput = new Scanner(System.in);
+         System.out.println("Enter a number to check perfect number");
+         int userInput = scanInput.nextInt();
+        int divisorSum = 0;
+        for (int i = 1; i < userInput; i++) {
+            if (userInput % i == 0) {
+                divisorSum = divisorSum + i;
+            }
+        }
+        if (divisorSum == userInput) {
+            System.out.println(userInput + " is a perfect number");
+        } else
+            System.out.println(userInput + " is not a perfect number");
+    }
+
     public static void main(String[] args) {
         JavaDay6Problems methodCaller = new JavaDay6Problems();
-        methodCaller.fibonacciSeries();
-    }
-    public void fibonacciSeries() {
-        Scanner fibonacciLengthInput = new Scanner(System.in);
-        System.out.println("Please enter the length of Fibonacci Series want to print for.");
-        int fibonacciInput =  fibonacciLengthInput.nextInt();
-        int number01 = 0;
-        int number02 = 1;
-        System.out.print(number01 + " + " + number02 + " + ");
-        for(int i=0;i < fibonacciInput-2;i++){
-            int number03 = number01 + number02;
-            System.out.print(" + "+number03);
-            number01 = number02;
-            number02 =  number03;
-        }
-
+        methodCaller.checkPerfectNumber();
     }
 }

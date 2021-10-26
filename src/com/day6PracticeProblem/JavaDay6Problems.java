2d330db24fd65ab0f87d1222d920558b421ec966
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-27 00:49:27 +0530

    Refactored code as to change method name.

--- src/com/day6PracticeProblem/JavaDay6Problems.java
+++ src/com/day6PracticeProblem/JavaDay6Problems.java
@@ -5,12 +5,12 @@ public class JavaDay6Problems {
     int flag;
     public static void main(String[] args) {
         JavaDay6Problems methodCaller = new JavaDay6Problems();
-        methodCaller.fibonacciSeries();
+        methodCaller.primeTest();
     }
-    public void fibonacciSeries() {
-        Scanner fibonacciLengthInput = new Scanner(System.in);
-        System.out.println("Please enter the length of Fibonacci Series want to print for.");
-        int primeInput =  fibonacciLengthInput.nextInt();
+    public void primeTest() {
+        Scanner scanInput = new Scanner(System.in);
+        System.out.println("Please enter a number to test if prime or not.");
+        int primeInput =  scanInput.nextInt();
         for (int i = 3; i < primeInput; i++) {
             if (primeInput % i == 0) {
                 flag = 1;

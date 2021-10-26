8b2225170402723378a16a7965b1ff6085ee0482
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-27 01:28:36 +0530

    Added code for Stopwatch Simulation.

--- src/com/day6PracticeProblem/JavaDay6Problems.java
+++ src/com/day6PracticeProblem/JavaDay6Problems.java
@@ -1,24 +1,49 @@
 package com.day6PracticeProblem;
+
 import java.util.*;
 
 public class JavaDay6Problems {
+
+    Scanner stopwatchInput = new Scanner(System.in);
+    double startTime, stopTime;
+
     public static void main(String[] args) {
         JavaDay6Problems methodCaller = new JavaDay6Problems();
-        methodCaller.fibonacciSeries();
+        methodCaller.swSimulator();
     }
-    public void fibonacciSeries() {
-        Scanner fibonacciLengthInput = new Scanner(System.in);
-        System.out.println("Please enter the length of Fibonacci Series want to print for.");
-        int fibonacciInput =  fibonacciLengthInput.nextInt();
-        int number01 = 0;
-        int number02 = 1;
-        System.out.print(number01 + " + " + number02 + " + ");
-        for(int i=0;i < fibonacciInput-2;i++){
-            int number03 = number01 + number02;
-            System.out.print(" + "+number03);
-            number01 = number02;
-            number02 =  number03;
-        }
 
+    public void swSimulator() {
+        JavaDay6Problems commonObject = new JavaDay6Problems();
+        commonObject.startMethod();
+        commonObject.stopMethod();
+        commonObject.printMethod();
+    }
+
+    private void startMethod() {
+        System.out.println("Please Enter 1 to start the Stopwatch.");
+        int start = stopwatchInput.nextInt();
+        if (start == 1) {
+            startTime = System.currentTimeMillis();
+        } else {
+            System.out.println("Invalid input please try again.");
+            startMethod();
+        }
+    }
+
+    private void stopMethod() {
+        System.out.println("Please Enter 0 to stop the Stopwatch.");
+        int stop = stopwatchInput.nextInt();
+        if (stop == 0) {
+            stopTime = System.currentTimeMillis();
+        } else {
+            System.out.println("Invalid input please try again.");
+            stopMethod();
+        }
+    }
+
+    private void printMethod() {
+        double timeInSeconds = ((stopTime - startTime)/1000);
+        System.out.println("Time in seconds is: " + timeInSeconds+" s");
     }
 }
+

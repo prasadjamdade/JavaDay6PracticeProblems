6b013f2d582e510da0438c4d73748ef9f5fd633a
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-26 11:01:40 +0530

    Welcome Print

--- src/com/day6PracticeProblem/JavaDay6Problems.java
+++ src/com/day6PracticeProblem/JavaDay6Problems.java
@@ -1,6 +1,6 @@
 package com.day6PracticeProblem;
 
-public class WelcomeNote {
+public class JavaDay6Problems {
     public static void main(String[] args) {
         System.out.println("\n\nPlease find solution to the problem statements for Day 6,\nplease switch through branches.");
     }
